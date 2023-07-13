8e452ca5fdc8326edd3ad0787243c015fbbc4cf6
Rafael Wortmann <wortmann@imis.uni-luebeck.de>
2023-07-13 23:54:49 +0200

    added splines between checkpoints

--- Assets/Scripts/routeManager.cs
+++ Assets/Scripts/routeManager.cs
@@ -5,26 +5,33 @@ using UnityEngine;
 using Unity.Mathematics;
 using UnityEngine.Splines;
 
-public class routeManager: MonoBehaviour
+public class routeManager : MonoBehaviour
 {
-    public double3[] routeCPs;
+    private Transform parent;
 
+    public double3[] routeCPs;
     public GameObject checkpointPrefab;
+    public GameObject splineRoutePrefab;
+    
 
     //todo auf Checkpoint ändern
     private GameObject[] checkpoints;
-
-    private Transform parent;
-
-
+    private GameObject routeSpline;
+  
     // Start is called before the first frame update
     void Start()
     {
-        //create CPs
-        checkpoints[0] = AddCheckpoint(routeCPs[0], "cp_1");
-        checkpoints[1] = AddCheckpoint(routeCPs[1], "cp_2");
+        
+        // create CPs
+        checkpoints = new GameObject[routeCPs.Length];
+        for (int i = 0; i < checkpoints.Length; i++)
+        {
+            checkpoints[i] = AddCheckpoint(routeCPs[i], "cp_" + i);
+        }
 
-        //draw splines
+        // create SplineRoute
+        createSplineGameObject();
+
     }
 
     // Update is called once per frame
@@ -43,18 +50,88 @@ public class routeManager: MonoBehaviour
 
         CesiumGlobeAnchor anchor = checkpoint.GetComponent<CesiumGlobeAnchor>();
         anchor.longitudeLatitudeHeight = position;
-        anchor.transform.localScale = new Vector3(400, 400, 400);
+        //anchor.transform.localScale = new Vector3(400, 400, 400);
 
         return checkpoint;
     }
 
-    void CreateSplines()
-    {
-        // Add a SplineContainer component to this GameObject.
-        var container = gameObject.AddComponent<SplineContainer>();
 
-        // Create a new Spline on the SplineContainer.
-        var spline = container.AddSpline();
+
+    void createSplineGameObject()
+    {
+        parent = this.transform.parent.transform;
+
+        // create spline from prefab
+        routeSpline = Instantiate(splineRoutePrefab);
+        routeSpline.name = "routeSpline";
+        routeSpline.transform.SetParent(parent);
+
+        // position spline object
+        CesiumGlobeAnchor anchor = routeSpline.GetComponent<CesiumGlobeAnchor>();
+        // using the position of the first cp as origin
+        anchor.longitudeLatitudeHeight = routeCPs[0];
+        anchor.transform.localScale = new Vector3(1, 1, 1);
+
+        // get spline component
+        var spline = routeSpline.GetComponent<SplineContainer>().Spline;
+
+        // get realativ positions
+        //float3[] relativPositions = getRelativPositionsIncludingTheEarthCurbing(routeCPs[0], routeCPs);
+        float3[] relativPositions = getRelativPositionsBetweenCheckpoints(checkpoints[0], checkpoints);
+
+        // create knots
+        BezierKnot[] cpKnots = new BezierKnot[relativPositions.Length];
+        for (int i = 1; i < routeCPs.Length; i++)
+        {
+            cpKnots[i] = new BezierKnot(relativPositions[i]);
+        }
+
+        // set knots to spline
+        spline.Knots = cpKnots;
+    }
+
+    /*
+     * This methods returns the realativ positions including the earth curbin, between one origin and an array of coordinates.
+     */
+    float3[] getRelativPositionsIncludingTheEarthCurbing(double3 origin, double3[] coords)
+    {
+
+        CesiumGeoreference cesiumGeoreference = GetComponentInParent<CesiumGeoreference>();
+        float3[] relativPositions = new float3[coords.Length];
+
+        for (int i = 1; i < coords.Length; i++)
+        {
+            // get realtiv position
+            double3 relativCoords = coords[i] - origin;
+
+            // convert 
+            double3 earthCenteredPosition = CesiumWgs84Ellipsoid.LongitudeLatitudeHeightToEarthCenteredEarthFixed(relativCoords);
+            double3 unityPosition = cesiumGeoreference.TransformEarthCenteredEarthFixedPositionToUnity(earthCenteredPosition);
+
+            relativPositions[i] = convertDouble3tofloat3(unityPosition);
+        }
+
+        return relativPositions;
+    }
+
+    float3[] getRelativPositionsBetweenCheckpoints(GameObject origin, GameObject[] coords) 
+    {
+        float3[] relativPositions = new float3[coords.Length];
+        for (int i = 1; i < coords.Length; i++)
+        {
+            // get realtiv position
+            relativPositions[i] = coords[i].transform.position - origin.transform.position;
+            relativPositions[i].y *= -0.4f;
+
+        }
+
+        return relativPositions;
+    }
+
+    float3 convertDouble3tofloat3(double3 vector)
+    {
+        return new float3((float)vector.x, (float)vector.y, (float)vector.z);
     }
 }
+
 
