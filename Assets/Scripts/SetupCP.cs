2a0f3fd83689ce2acca97da8172df8090ac4c7bd
Rafael Wortmann <wortmann@imis.uni-luebeck.de>
2023-07-17 23:10:18 +0200

    added cylinders to cp spheres

--- Assets/Scripts/SetupCP.cs
+++ Assets/Scripts/SetupCP.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class SetupCP : MonoBehaviour
 {
+    private Transform parent;
+    public float radius = 50f;
+    public float height = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
-      
+        parent = this.transform;
+
+        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        cylinder.transform.SetParent(parent);
+        cylinder.name = "cp_location_marker";
+        cylinder.transform.position = new Vector3(0f,0f,0f);
+        cylinder.transform.localScale = new Vector3(radius * 2f, height, radius * 2f);
     }
 
     // Update is called once per frame
